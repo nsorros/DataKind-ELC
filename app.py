01540fa16e45192a245c6f3a9b02b75f561671a3
Nick Sorros <nsorros@gmail.com>
2016-06-17 21:02:21 +0100

    added demo resource

--- app.py
+++ app.py
@@ -3,15 +3,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
-from json import dumps
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 class Lsoa(Resource):
     def get(self, lsoa_code):
-        #Connect to database
-		e = create_engine('sqlite:////media/windows-share/db_landcat.sqlite')
+        # Connect to database
+        e = create_engine('sqlite:////media/windows-share/db_landcat.sqlite')
         conn = e.connect()
         
         #Perform query and return JSON data
@@ -21,7 +21,12 @@ class Lsoa(Resource):
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return result
 
+class Demo(Resource):
+    def get(self):
+        data = json.loads('farms_geo.json')
+        return data
 
+api.add_resource(Demo, '/', '/demo', '/demo/')
 api.add_resource(Lsoa, '/lsoa/<string:lsoa_code>')
 
 if __name__ == '__main__':
